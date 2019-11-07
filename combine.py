aa87503751d41f2f805d519cec0d0e754aee04af
will <will@yager.io>
2019-11-07 14:35:45 -0800

    ok

--- combine.py
+++ combine.py
@@ -3,14 +3,24 @@ import imageio
 import cv2
 import sys
 
+# ORB works poorly with lots of dark pixels,
+# probably on account of uint8 quantization to zero.
+# This is a very simple brightener - you don't want to 
+# use this for the final result
+def brighten(img,threshold=0.1): 
+    if img.mean() > threshold:
+        return img
+    else:
+        return brighten(2*img - img**2,threshold) # Closes over [0,1]
+        
 # compute a homography (3x3 transform matrix for 2D homogenous coordinates)
 # that projects image 1 onto image 2. We use this to line up multiple images
 # of the sky that were taken at different times, with the camera at a slightly
 # different angle, etc.
 def match(im1,im2):
     # OpenCV ORB requires uint8 for some reason
-    im1 = (im1*256).astype('uint8') 
-    im2 = (im2*256).astype('uint8')
+    im1 = (brighten(im1)*255).astype('uint8') 
+    im2 = (brighten(im2)*255).astype('uint8')
     # Identify interesting points in the image (i.e. stars)
     det = cv2.ORB_create(nfeatures=50000)
     kp1,desc1 = det.detectAndCompute(im1,None)
@@ -31,7 +41,7 @@ def match(im1,im2):
     # Mask tells us which matches seem internally consistent.
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
     if mask.sum() < 10:
-        raise Exception("<10 RANSAC inlier descriptors, poor match")
+       raise Exception("<10 RANSAC inlier descriptors, poor match")
     return M
 
 # Using `im` as the canvas, project every image in `loaders` onto `im` (after lining them up).
@@ -40,7 +50,7 @@ def match(im1,im2):
 # We use `loaders` instead of passing in images directly because we want to 
 # load huge TIFF images into memory one at a time rather than all at once.
 def add(im,loaders):
-    (h,w,_) = im.shape
+    h,w,*_ = im.shape
     out = im.copy()
     count = np.full((h,w),1.0)
     for load_im2 in loaders:
